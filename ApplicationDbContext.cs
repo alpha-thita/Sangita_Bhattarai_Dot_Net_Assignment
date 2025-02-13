c95f140b79559b2fd8c0eb49d532aba227576f89
Sangita <166576573+alpha-thita@users.noreply.github.com>
2025-02-13 23:30:39 +0545

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineStoreFurniture.Models;
+
+namespace OnlineStoreFurniture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<order> Orders { get; set; }
+        public DbSet<orderItem> OrderItems { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Exclude the existing Identity table from migrations
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRoles", t => t.ExcludeFromMigrations());
+        }
+    }
+}
